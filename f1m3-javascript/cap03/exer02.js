a5d8823b95080a45389158a1f6c68ca72772f7cb
Anderson Gomes <dev.andersong@gmail.com>
2023-10-07 09:15:41 -0300

    Alteração nos comentários

--- f1m3-javascript/cap03/exer02.js
+++ f1m3-javascript/cap03/exer02.js
@@ -1,10 +1,10 @@
-/*
-    Comandos de Repetição: Exercício 02. Faça um algoritmo que conte de 1 a 100 e a cada múltiplo de 10 emita uma mensagem: “Múltiplo de 10: <número>”. (neste caso, considerrando o intervalo de 1 até 20).
-*/
+
+// Comandos de Repetição: Exercício 02. Faça um algoritmo que conte de 1 a 100 e a cada múltiplo de 10 emita uma mensagem: “Múltiplo de 10: <número>”. (neste caso, considerrando o intervalo de 1 até 20).
+
 
 let num = 0;
 for (let i = 1; i <= 100; i++) {
-    if (i % 10 == 0){
+    if (i % 10 == 0) {
         num = i
         console.log("Múltiplo de 10:", num);
     } else {
